6efcde65559d60afbaf42eed9345849b1cf43796
Dima <uvarov3123@gmail.com>
2024-02-02 17:38:51 +0200

    add blocks

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -8,7 +8,7 @@
 // При необхідності підключаємо додаткові модулі слайдера, вказуючи їх у {} через кому
 // Приклад: { Navigation, Autoplay }
 import Swiper from "swiper"
-import { Pagination } from "swiper/modules"
+import { Navigation, Pagination, Autoplay } from "swiper/modules"
 /*
 Основні модулі слайдера:
 Navigation, Pagination, Autoplay,
@@ -30,7 +30,7 @@ function initSliders() {
   // Перевіряємо, чи є слайдер на сторінці
   if (document.querySelector(".service__slider")) {
     new Swiper(".service__slider", {
-      modules: [Pagination],
+      modules: [Navigation],
       observer: true,
       observeParents: true,
 
@@ -55,10 +55,10 @@ function initSliders() {
 
       // Пагінація
 
-      pagination: {
-        el: ".service__pagination",
-        clickable: true,
-      },
+      //   pagination: {
+      //     el: ".service__pagination",
+      //     clickable: true,
+      //   },
 
       // Скроллбар
       /*
@@ -69,10 +69,10 @@ function initSliders() {
 			*/
 
       // Кнопки "вліво/вправо"
-      // navigation: {
-      // 	prevEl: '.swiper-button-prev',
-      // 	nextEl: '.swiper-button-next',
-      // },
+      navigation: {
+        prevEl: ".service__slider .button-prev",
+        nextEl: ".service__slider .button-next",
+      },
 
       // Брейкпоінти
       breakpoints: {
@@ -88,12 +88,12 @@ function initSliders() {
         },
         992: {
           slidesPerView: "auto",
-          spaceBetween: 40,
+          spaceBetween: 30,
           autoHeight: false,
         },
         1230: {
-          slidesPerView: "auto",
-          spaceBetween: 70,
+          slidesPerView: 3,
+          spaceBetween: 45,
           autoHeight: false,
         },
       },
@@ -101,6 +101,122 @@ function initSliders() {
       // Події
       on: {},
     })
+  }
+  if (document.querySelector(".we-work__slider")) {
+    new Swiper(".we-work__slider", {
+      modules: [Navigation],
+      observer: true,
+      observeParents: true,
+      speed: 800,
+
+      // Кнопки "вліво/вправо"
+      navigation: {
+        prevEl: ".we-work__slider .button-prev",
+        nextEl: ".we-work__slider .button-next",
+      },
+
+      // Брейкпоінти
+      breakpoints: {
+        320: {
+          slidesPerView: 1.2,
+          spaceBetween: 20,
+          //   autoHeight: true,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+          //   autoHeight: false,
+        },
+        992: {
+          slidesPerView: 2,
+          spaceBetween: 40,
+          //   autoHeight: false,
+        },
+        1230: {
+          slidesPerView: 1.8,
+          spaceBetween: 70,
+          //   autoHeight: false,
+        },
+      },
+    })
+  }
+  if (document.querySelector(".stages__slider")) {
+    new Swiper(".stages__slider", {
+      modules: [Navigation],
+      observer: true,
+      observeParents: true,
+      speed: 800,
+
+      // Кнопки "вліво/вправо"
+      navigation: {
+        prevEl: ".stages__slider .button-prev",
+        nextEl: ".stages__slider .button-next",
+      },
+
+      // Брейкпоінти
+      breakpoints: {
+        320: {
+          slidesPerView: "auto",
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: "auto",
+          spaceBetween: 20,
+        },
+        992: {
+          slidesPerView: "auto",
+          spaceBetween: 40,
+        },
+        1230: {
+          slidesPerView: "auto",
+          spaceBetween: 70,
+        },
+      },
+    })
+  }
+
+  for (const mobileSlider of document.querySelectorAll(".partners__slider")) {
+    if (mobileSlider) {
+      ;(function () {
+        "use strict"
+
+        const breakpoint = window.matchMedia("(min-width:768px)")
+        let slider
+
+        const enableSwiper = function () {
+          slider = new Swiper(mobileSlider, {
+            modules: [Autoplay],
+            observer: true,
+            observeParents: true,
+            slidesPerView: 2.4,
+            spaceBetween: 30,
+
+            speed: 1500,
+            loop: true,
+            loopAdditionalSlides: 5,
+            allowTouchMove: false,
+
+            autoplay: {
+              delay: 0,
+              disableOnInteraction: false,
+            },
+          })
+        }
+
+        const breakpointChecker = function () {
+          if (breakpoint.matches === true) {
+            if (slider !== undefined) slider.destroy(true, true)
+
+            return
+          } else if (breakpoint.matches === false) {
+            return enableSwiper()
+          }
+        }
+
+        breakpoint.addListener(breakpointChecker)
+        breakpointChecker()
+      })()
+    }
   }
 }
 // Скролл на базі слайдера (за класом swiper scroll для оболонки слайдера)
